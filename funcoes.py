06fd5a5d031f19145436e140dbff21dd7d39d146
Jean <mrjean.hw@gmail.com>
2025-09-01 20:20:35 -0300

    Otimizando

--- funcoes.py
+++ funcoes.py
@@ -1,6 +1,6 @@
 def impar_par(numero: int) -> str:
-    """Verifica se um número é par ou ímpar."""
+    '''Verifica se um número é par ou ímpar.'''
     if numero % 2 == 0:
-        return f"{numero} é Par"
+        return f'{numero} é Par'
     else:
-        return f"{numero} é Ímpar"
+        return f'{numero} é Ímpar'
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import funcoes as f
 
 def main():
-    numero = int(input("Digite um número: "))
+    numero = int(input('Digite um número: '))
     print(f.impar_par(numero))
 main()
