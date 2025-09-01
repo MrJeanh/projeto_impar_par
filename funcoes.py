4cc3ba128753432c877b43444dd9dcc3bae84ed7
Jean <mrjean.hw@gmail.com>
2025-08-31 21:03:48 -0300

    Primeiro commit do projeto impar_par

---
+++ funcoes.py
@@ -0,0 +1,6 @@
+def impar_par(numero: int) -> str:
+    """Verifica se um número é par ou ímpar."""
+    if numero % 2 == 0:
+        return f"{numero} é Par"
+    else:
+        return f"{numero} é Ímpar"
---
+++ main.py
@@ -0,0 +1,8 @@
+from funcoes import impar_par
+
+def main():
+    numero = int(input("Digite um número: "))
+    print(impar_par(numero))
+
+if __name__ == "__main__":
+    main()
