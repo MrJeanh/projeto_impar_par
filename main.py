d481ca83d514afc3a26b99c10e698b33861bc8c8
Jean <mrjean.hw@gmail.com>
2025-09-16 21:05:50 -0300

    Aprimorando

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import funcoes as f
+"""
+Este programa solicita números do usuário,
+e verifica se o número digitado é ímpar ou par.
+Exemplo: 5 = 5 é Ímpar.
+"""
+
 def main():
     numero = int(input('Digite um número: '))
     print(f.impar_par(numero))
